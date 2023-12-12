c52b9837b1833a6eba854941621dc89e1aa13a39
frederic-nevers <fredericnevers@gmail.com>
2023-12-12 23:25:40 +0800

    Update URLs and file paths

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,15 +19,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://digsoc.fredericnevers.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'frederic-nevers', // Usually your GitHub org/user name.
+  projectName: 'digsocrevision', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -100,7 +100,7 @@ const config = {
             items: [
               {
                 label: 'Revision',
-                to: '/docs/intro',
+                to: '/docs/1-Intro',
               },
             ],
           },
---
+++ exams/intro.md
@@ -0,0 +1,6 @@
+---
+sidebar_position: 1
+id: 1-Intro
+title: 1. Introduction to Digital Society
+---
+
